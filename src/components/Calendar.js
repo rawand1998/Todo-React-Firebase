12529e5300beca66c30d30d285f22a82e96690de
rawand1998 <rawandgaradh@gmail.com>
2022-06-14 23:05:17 +0300

    calender component and it style

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,12 +1,28 @@
-import React from 'react'
+import React from "react";
+import { CalendarDate, CaretUp } from "react-bootstrap-icons";
 
-function Calendar(){
-
-    return (
-        <div className='Calendar'>
-            Calendar
+function Calendar() {
+  const calendarItems = ["today", "next 7 days", "all days"];
+  return (
+    <div className="Calendar">
+      <div className="header">
+        <div className="title">
+          <CalendarDate />
+          <p>Calendar</p>
         </div>
-    )
+        <div className="btn">
+         <span><CaretUp /></span> 
+        </div>
+      </div>
+      <div className="items">
+        {calendarItems.map((item) => (
+          <div className="item" key={item}>
+            {item}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default Calendar
+export default Calendar;
